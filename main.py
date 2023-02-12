048bac75f1bc015cf604b69ffee6438d24215b8c
omerday <omer.dayan4u@gmail.com>
2023-02-12 09:48:18 +0200

    Instructions Start

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from psychopy import visual, core, event
-from psychopy import parallel
+import helpers
 import runConfigDialog
 
 configDialogBank = runConfigDialog.userInputPlay()
 
 params = {
     'subjectID': configDialogBank[0],
-    'practiceTrials': configDialogBank[1],
-    'numOfScreensTask1': configDialogBank[2],
-    'numOfScreensTask2': configDialogBank[3],
-    'startingDistance': configDialogBank[4],
+    'practiceTrials': configDialogBank[1],  # Number if Practice Trials, taken from Config Dialog
+    'numOfScreensTask1': configDialogBank[2],  # Number of Screens in the 1st task, either 49 (7*7) or 36 (6*6)
+    'numOfScreensTask2': configDialogBank[3],  # Number of Screens in the 2nd task, either 49 (7*7) or 36 (6*6)
+    'startingDistance': configDialogBank[4],  # Decide whether the starting distance is random, or fixed on 50
     'fullScreen': configDialogBank[5],
     'joystickSensitivity': configDialogBank[6],
     'screenSize': (1024, 768),
     # 'portAddress': int("0xE050", 16)
 }
 
-
-win = visual.Window(params['screenSize'], monitor="testMonitor",color="black",winType='pyglet')
-img = visual.ImageStim(win=win, image="./img/ITI_fixation.jpg", units="pix", opacity=1, size=(params[ 'screenSize'][0], params['screenSize'][1]))
+# Initialize Screen
+win = visual.Window(params['screenSize'], monitor="testMonitor", color="black", winType='pyglet')
+img = visual.ImageStim(win=win, image="./img/ITI_fixation.jpg", units="pix", opacity=1,
+                       size=(params['screenSize'][0], params['screenSize'][1]))
 img.draw()
 win.update()
-core.wait(5)
+helpers.waitForSpace()
+# core.wait(5)
