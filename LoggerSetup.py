a345283ec4bd4fd18cda034f80aec9f3a3bdbbae
yovelP <125245689+yovelP@users.noreply.github.com>
2023-02-14 15:28:40 +0200

    start

--- LoggerSetup.py
+++ LoggerSetup.py
@@ -2,8 +2,11 @@ import datetime
 import logging
 
 def set_up_logger():
+    # logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
+    #                     filename="./Logs/" + str(datetime.datetime.now()), filemode='w', level='DEBUG')
+    date = str(datetime.datetime.now().date())
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
-                        filename="./Logs/" + str(datetime.datetime.now()), filemode='w', level='DEBUG')
+                        filename="./Logs/" + date, filemode='w', level='DEBUG')
     logger = logging.getLogger(__name__)
     logger.info("Initiating Logger")
     return logger
